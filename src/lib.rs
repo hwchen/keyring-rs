68975839332e3dff0a5a3113a35ff5fb5e09a523
Walther Chen <walther.chen@gmail.com>
2019-04-19 22:30:53 -0400

    update to secret-service 1.0, move tests to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -37,6 +37,11 @@ pub use error::{KeyringError, Result};
 mod tests {
     use super::*;
 
+    static TEST_SERVICE: &'static str = "test.keychain-rs.io";
+    static TEST_USER: &'static str = "user@keychain-rs.io";
+    static TEST_ASCII_PASSWORD: &'static str = "my_password";
+    static TEST_NON_ASCII_PASSWORD: &'static str = "大根";
+
     #[test]
     fn test_empty_password_input() {
         let pass = "";
@@ -45,5 +50,49 @@ mod tests {
         let out = keyring.get_password().unwrap();
         keyring.delete_password().unwrap();
         assert_eq!(pass, out);
+    }
+
+    #[test]
+    fn test_add_ascii_password() {
+        let keyring = Keyring::new(TEST_SERVICE, TEST_USER);
+
+        keyring.set_password(TEST_ASCII_PASSWORD).unwrap();
+
+        keyring.delete_password().unwrap();
+    }
+
+    #[test]
+    fn test_round_trip_ascii_password() {
+        let keyring = Keyring::new(TEST_SERVICE, TEST_USER);
+
+        keyring.set_password(TEST_ASCII_PASSWORD).unwrap();
+
+        let stored_password = keyring.get_password().unwrap();
+
+        assert_eq!(stored_password, TEST_ASCII_PASSWORD);
+
+        keyring.delete_password().unwrap();
+    }
+
+    #[test]
+    fn test_add_non_ascii_password() {
+        let keyring = Keyring::new(TEST_SERVICE, TEST_USER);
+
+        keyring.set_password(TEST_NON_ASCII_PASSWORD).unwrap();
+
+        keyring.delete_password().unwrap();
+    }
+
+    #[test]
+    fn test_round_trip_non_ascii_password() {
+        let keyring = Keyring::new(TEST_SERVICE, TEST_USER);
+
+        keyring.set_password(TEST_NON_ASCII_PASSWORD).unwrap();
+
+        let stored_password = keyring.get_password().unwrap();
+
+        assert_eq!(stored_password, TEST_NON_ASCII_PASSWORD);
+
+        keyring.delete_password().unwrap();
     }
 }
