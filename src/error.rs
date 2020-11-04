d05acc6680e8078e34459bb822501c6164edb346
Ben Scott <gamepoet@gmail.com>
2020-11-04 15:47:34 -0800

    Remove usage of deprecated Error API (description and cause).

--- src/error.rs
+++ src/error.rs
@@ -40,21 +40,7 @@ impl fmt::Display for KeyringError {
 }
 
 impl error::Error for KeyringError {
-    fn description(&self) -> &str {
-        match *self {
-            #[cfg(target_os = "macos")]
-            KeyringError::MacOsKeychainError(ref err) => err.description(),
-            #[cfg(target_os = "linux")]
-            KeyringError::SecretServiceError(ref err) => err.description(),
-            #[cfg(target_os = "windows")]
-            KeyringError::WindowsVaultError => "Windows Vault Error",
-            KeyringError::NoBackendFound => "No Backend Found",
-            KeyringError::NoPasswordFound => "No Password Found",
-            KeyringError::Parse(ref err) => err.description(),
-        }
-    }
-
-    fn cause(&self) -> Option<&dyn error::Error> {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match *self {
             #[cfg(target_os = "linux")]
             KeyringError::SecretServiceError(ref err) => Some(err),
