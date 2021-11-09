f47b82c8f3212910a055352798a9ade0f23e3e6e
Daniel Brotsky <dev@brotsky.com>
2021-11-09 05:17:18 -0800

    Restore testing on all platforms
    Now that linux testing works, put back the testing on all platforms.
    
    (Also did one tweak to the linux test script: don't replace any existing gnome-keyring session.)

--- linux-test.sh
+++ linux-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rm -f $HOME/.local/share/keyrings/*
-echo -n "test" | gnome-keyring-daemon --replace --unlock
+echo -n "test" | gnome-keyring-daemon --unlock
 cargo test --verbose
