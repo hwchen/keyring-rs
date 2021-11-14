3d16934f89c70d13f0479dff9db979d20e878fec
Daniel Brotsky <dbrotsky@adobe.com>
2021-11-14 00:02:49 -0800

    Fix typo; improve example code.
    1. There was a missing method in the decode_error function on Mac.
    2. Improve the examples to show how errors are described.

--- examples/example.rs
+++ examples/example.rs
@@ -7,13 +7,19 @@ fn main() -> Result<()> {
     let service = "example-service";
     let password = "example-password";
     let keyring = Keyring::new(service, username);
-    keyring.set_password(password)?;
-    let stored_password = keyring.get_password()?;
-    assert_eq!(
-        password, stored_password,
-        "Stored and retrieved passwords don't match"
-    );
-    keyring.delete_password()?;
+    if let Err(err) = keyring.set_password(password) {
+        panic!("Could not set password: {}", err)
+    }
+    match keyring.get_password() {
+        Ok(stored_password) => assert_eq!(
+            password, stored_password,
+            "Stored and retrieved passwords don't match"
+        ),
+        Err(err) => panic!("Could not get password: {}", err),
+    }
+    if let Err(err) = keyring.delete_password() {
+        panic!("Could not delete password: {}", err);
+    }
     assert!(
         keyring.get_password().is_err(),
         "No error retrieving password after deletion"
