01ebb89a88e3f9729bacc2081f3313560249985c
Daniel Brotsky <dev@brotsky.com>
2021-11-21 15:18:47 -0800

    Add verbose mode to the CLI.
    Also remove the `clap` dependency, now that we are using structopt.

--- examples/cli.rs
+++ examples/cli.rs
@@ -5,8 +5,8 @@ extern crate keyring;
 use keyring::{Entry, Error};
 
 #[derive(Debug, StructOpt)]
-#[structopt(about = "A utility to access platform secure storage")]
-pub struct KeyringCli {
+/// Keyring CLI: A command-line interface to platform secure storage
+pub struct Cli {
     #[structopt(short, parse(from_occurrences))]
     /// Specify once to retrieve all aspects of credentials on get.
     /// Specify twice to provide structure print of all errors in addition to messages.
@@ -29,7 +29,6 @@ pub struct KeyringCli {
 }
 
 #[derive(Debug, StructOpt)]
-/// Keyring CLI
 pub enum Command {
     /// Set the password in the secure store
     Set {
@@ -44,52 +43,97 @@ pub enum Command {
 }
 
 fn main() {
-    let args: KeyringCli = KeyringCli::from_args();
+    let args: Cli = Cli::from_args();
     execute_args(&args);
 }
 
-fn execute_args(args: &KeyringCli) {
+fn execute_args(args: &Cli) {
     let keychain = args.keychain.clone();
     let username = args.username.clone().unwrap_or_else(whoami::username);
     let entry = Entry::new_in_keychain(&keychain, &args.service, &username);
     match &args.command {
         Command::Set {
             password: Some(password),
-        } => execute_set_password(&entry, password),
+        } => execute_set_password(args, &entry, password),
         Command::Set { password: None } => {
             if let Ok(password) = read_password_from_tty(Some("Password: ")) {
-                execute_set_password(&entry, &password)
+                execute_set_password(args, &entry, &password)
             } else {
                 eprintln!("(Failed to read password, so none set.)")
             }
         }
-        Command::Get => execute_get_password(&entry),
-        Command::Delete => execute_delete_password(&entry),
+        Command::Get => execute_get_password_and_credential(args, &entry),
+        Command::Delete => execute_delete_password(args, &entry),
     }
 }
 
-fn execute_set_password(entry: &Entry, password: &str) {
+fn execute_set_password(args: &Cli, entry: &Entry, password: &str) {
     match entry.set_password(password) {
         Ok(()) => println!("Password set successfully"),
-        Err(Error::NoStorageAccess(err)) => eprintln!("Couldn't set the password: {}", err),
-        Err(err) => eprintln!("Unexpected error setting the password: {}", err),
+        Err(Error::NoStorageAccess(err)) => {
+            eprintln!("Couldn't set the password: {}", err);
+            if args.verbose > 1 {
+                eprintln!("Error details: {:?}", err);
+            }
+        }
+        Err(err) => {
+            eprintln!("Unexpected error setting the password: {}", err);
+            if args.verbose > 1 {
+                eprintln!("Error details: {:?}", err);
+            }
+        }
     }
 }
 
-fn execute_get_password(entry: &Entry) {
-    match entry.get_password() {
-        Ok(password) => println!("Password is '{}'", &password),
-        Err(Error::NoEntry) => eprintln!("(No password found)"),
-        Err(Error::NoStorageAccess(err)) => eprintln!("Couldn't retrieve the password: {}", err),
-        Err(err) => eprintln!("Unexpected error retrieving the password: {}", err),
+fn execute_get_password_and_credential(args: &Cli, entry: &Entry) {
+    match entry.get_password_and_credential() {
+        Ok((password, credential)) => {
+            println!("Password is '{}'", &password);
+            if args.verbose > 0 {
+                println!("Credential is: {:?}", credential)
+            }
+        }
+        Err(Error::NoEntry) => {
+            eprintln!("(No password found)");
+            if args.verbose > 1 {
+                eprintln!("Error details: {:?}", Error::NoEntry);
+            }
+        }
+        Err(Error::NoStorageAccess(err)) => {
+            eprintln!("Couldn't retrieve the password: {}", err);
+            if args.verbose > 1 {
+                eprintln!("Error details: {:?}", err);
+            }
+        }
+        Err(err) => {
+            eprintln!("Unexpected error retrieving the password: {}", err);
+            if args.verbose > 1 {
+                eprintln!("Error details: {:?}", err);
+            }
+        }
     }
 }
 
-fn execute_delete_password(entry: &Entry) {
+fn execute_delete_password(args: &Cli, entry: &Entry) {
     match entry.delete_password() {
         Ok(()) => println!("(Password deleted)"),
-        Err(Error::NoEntry) => eprintln!("(No password found)"),
-        Err(Error::NoStorageAccess(err)) => eprintln!("Couldn't delete the password: {}", err),
-        Err(err) => eprintln!("Unexpected error retrieving the password: {}", err),
+        Err(Error::NoEntry) => {
+            eprintln!("(No password found)");
+            if args.verbose > 1 {
+                eprintln!("Error details: {:?}", Error::NoEntry);
+            }
+        }
+        Err(Error::NoStorageAccess(err)) => {
+            eprintln!("Couldn't delete the password: {}", err);
+            if args.verbose > 1 {
+                eprintln!("Error details: {:?}", err);
+            }
+        }
+        Err(err) => {
+            eprintln!("Unexpected error retrieving the password: {}", err);
+            if args.verbose > 1 {
+                eprintln!("Error details: {:?}", err);
+            }
+        }
     }
 }
