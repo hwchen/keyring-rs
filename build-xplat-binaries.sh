5b9d559d83df1e45ddb9201086bed51d2f5c41c2
Daniel Brotsky <dan@brotsky.com>
2023-12-25 19:19:34 -0800

    Rework default feature set.
    This fixes #153 by making all platform dependencies be features and including them as default features, so now you can suppress building all platform dependencies by suppressing default features.
    
    This also fixes #148 by having the code check to see if there is platform support and, if not, using the mock keystore as the default.
    
    While this change is not fully backward compatible for people who were suppressing default features on macOS, iOS, Win, or FreeBSD, doing so was undefined behavior on those platforms and so changing it does not break the semantics of the API.

--- build-xplat-binaries.sh
+++ build-xplat-binaries.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+echo Rustup and Cargo updates...
 rustup update
 cargo update
-cargo clippy --target x86_64-unknown-linux-musl
-cargo clippy --target x86_64-pc-windows-gnu
+echo Clippy no default features...
+cargo clippy --no-default-features --target aarch64-unknown-linux-musl
+cargo clippy --no-default-features --target aarch64-pc-windows-msvc
+cargo clippy --no-default-features --target aarch64-apple-darwin
+cargo clippy --no-default-features --target aarch64-apple-ios
+echo Clippy default features...
+cargo clippy --target aarch64-unknown-linux-musl
+cargo clippy --target aarch64-pc-windows-msvc
 cargo clippy --target aarch64-apple-darwin
 cargo clippy --target aarch64-apple-ios
-cargo build --target x86_64-unknown-linux-musl
-cargo build --target x86_64-pc-windows-gnu
+echo Compile no default features...
+cargo build --no-default-features --target aarch64-unknown-linux-musl
+cargo build --no-default-features --target aarch64-pc-windows-msvc
+cargo build --no-default-features --target aarch64-apple-darwin
+cargo build --no-default-features --target aarch64-apple-ios
+echo Compile default features...
+cargo build --target aarch64-unknown-linux-musl
+cargo build --target aarch64-pc-windows-msvc
 cargo build --target aarch64-apple-darwin
 cargo build --target aarch64-apple-ios
--- build-xplat-docs.sh
+++ build-xplat-docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cargo doc --no-deps --target x86_64-unknown-linux-musl $OPEN_DOCS
-cargo doc --no-deps --target x86_64-pc-windows-gnu $OPEN_DOCS
+cargo doc --no-deps --target aarch64-unknown-linux-musl $OPEN_DOCS
+cargo doc --no-deps --target aarch64-pc-windows-msvc $OPEN_DOCS
 cargo doc --no-deps --target aarch64-apple-darwin $OPEN_DOCS
 cargo doc --no-deps --target aarch64-apple-ios $OPEN_DOCS
